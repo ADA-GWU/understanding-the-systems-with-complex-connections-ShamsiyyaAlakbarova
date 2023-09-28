5c480e86335862da4eb05c46c94bc762d60dff8a
ShamsiyyaAlakbarova <shamsnajafli@gmail.com>
2023-09-29 00:10:14 +0400

    added 3 ports

--- client.py
+++ client.py
@@ -1,13 +1,61 @@
-import socket
+"""import socket
 
 s = socket.socket()
+print('Socket created')
+
 port = 9001
 s.connect(('127.0.0.1', port))
 
-# Get a number from the user
-number = input("Enter a number: ")
+while True:
+    # Get a number from the user
+    number = input("Enter a number (or 'exit' to quit): ")
+    
+    if number.lower() == 'exit':
+        break
 
-# Send the number to the server
-s.send(number.encode())
+    # Send the number to the server
+    s.send(number.encode())
 
 s.close()
+"""
+
+import socket
+import sys
+
+s = socket.socket()
+
+if len(sys.argv) != 2:
+    print("Usage: python client.py <port>")
+    sys.exit(1)
+
+try:
+    port = int(sys.argv[1])
+except ValueError:
+    print("Invalid port number")
+    sys.exit(1)
+
+print(f'Socket created for port {port}')
+
+server_address = ('127.0.0.1', port)
+
+try:
+    s.connect(server_address)
+    print(f'Connected to server on port {port}')
+
+    while True:
+        number = input("Enter a number (or 'exit' to quit): ")
+
+        if number.lower() == 'exit':
+            break
+
+        s.send(number.encode())
+
+        result = s.recv(1024).decode()
+
+
+except ConnectionRefusedError:
+    print("Connection refused. Make sure the server is running.")
+except KeyboardInterrupt:
+    print("Client is closing.")
+finally:
+    s.close()
--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket
+"""import socket
 
 s = socket.socket()
 print('Socket created')
@@ -14,11 +14,63 @@ while True:
     c, addr = s.accept()
     print('Got connection from', addr)
 
-    number = c.recv(1024).decode()
+    while True:
+        number = c.recv(1024).decode()
+
+        if not number:
+            break
+        
+        # Double the number
+        result = str(int(number) * 2)
+
+        print('Result from server:', result)
+
+        c.send(result.encode())
+
+    c.close()"""
+
+import socket
+
+def handle_client(client_socket):
+    while True:
+        number = client_socket.recv(1024).decode()
+
+        if not number:
+            break
+
+        # Double the number
+        result = str(int(number) * 2)
+
+        print('Result from client:', result)
+
+        client_socket.send(result.encode())
+
+    client_socket.close()
+
+# Create sockets and bind them to specific ports
+ports = [9001, 9002, 9003]
+sockets = []
+
+for port in ports:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', port))
+    s.listen(5)  # You can adjust the backlog queue size as needed
+    sockets.append(s)
+    print(f'Socket bound to port {port}')
+
+print('Server is listening on multiple ports')
+
+while True:
+    for i, s in enumerate(sockets):
+        c, addr = s.accept()
+        print(f'Got connection on port {ports[i]} from', addr)
+        handle_client(c)
+
+
+
     
-    # Double the number 
-    result = str(int(number) * 2)
-    
-    print('Result from server:', result)
-    
-    c.close()
+
+
+
+
+
